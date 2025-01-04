18f34f279ed113a1c6657906401afa1e67c36379
Bálint Király <balintkiraly.dev@gmail.com>
2025-01-04 23:56:33 +0100

    Main heading

--- src/components/sections/LandingSection.tsx
+++ src/components/sections/LandingSection.tsx
@@ -1,3 +1,4 @@
+import { ChevronDown } from 'lucide-react';
 import Image from 'next/image';
 import React from 'react';
 
@@ -5,7 +6,7 @@ import Countdown from '@/components/ui/Countdown';
 
 const LandingSection = () => {
   return (
-    <section className='flex h-screen flex-col items-center justify-center text-4xl text-white'>
+    <section className='flex h-screen flex-col items-center justify-end text-4xl text-white'>
       <div className='absolute -z-10 h-screen overflow-hidden'>
         <Image
           className='relative -top-20'
@@ -16,8 +17,13 @@ const LandingSection = () => {
         />
       </div>
       <div className='absolute inset-0 -z-10 bg-gradient-to-t from-black via-black/70 to-transparent' />
+      <h1 className='absolute left-5 top-20 max-w-3xl text-[5rem]/[1] font-bold'>
+        Welcome to the <br />
+        instructor training!
+      </h1>
       <Countdown targetDate={process.env.COUNTDOWN_TARGET ?? new Date().toDateString()} />
-      <button>Join Now</button>
+      <button>Apply Now</button>
+      <ChevronDown />
     </section>
   );
 };
