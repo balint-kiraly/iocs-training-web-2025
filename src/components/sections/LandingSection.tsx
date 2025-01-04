c35384f34ee9231454ba69d62f18b07f18aaa9e8
Bálint Király <balintkiraly.dev@gmail.com>
2025-01-04 20:12:09 +0100

    Background placeholder

--- src/components/sections/LandingSection.tsx
+++ src/components/sections/LandingSection.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 import Countdown from '@/components/ui/Countdown';
@@ -5,6 +6,16 @@ import Countdown from '@/components/ui/Countdown';
 const LandingSection = () => {
   return (
     <section className='h-screen'>
+      <div className='absolute -z-10 h-screen overflow-hidden'>
+        <Image
+          className='relative -top-20'
+          src='/landing-bg-placeholder.jpg'
+          width={5427}
+          height={3618}
+          alt='Background placeholder'
+        />
+      </div>
+      <div className='absolute inset-0 bg-gradient-to-t from-black via-black/70 to-transparent' />
       <h1>Landing Section</h1>
       <Countdown />
       <button>Join Now</button>
