31156675afbf31369c4e397904b06d2681967db3
Nagy Tamás <tomcsi01203@gmail.com>
2025-01-05 01:44:41 +0100

    Added Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,136 +1,55 @@
-import Image from 'next/image';
+import { Facebook, Instagram } from "lucide-react";
 
-export default function Home() {
+export default function Footer(){
   return (
-    <div
-      className={`
-        grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20
+    <header className="flex justify-between items-center p-4">
 
-        font-[family-name:var(--font-geist-sans)]
-
-        sm:p-20
-      `}
-    >
-      <main
-        className={`
-          row-start-2 flex flex-col items-center gap-8
-
-          sm:items-start
-        `}
-      >
-        <Image className='dark:invert' src='/next.svg' alt='Next.js logo' width={180} height={38} priority />
-        <ol
-          className={`
-            list-inside list-decimal text-center text-sm
-
-            font-[family-name:var(--font-geist-mono)]
-
-            sm:text-left
-          `}
-        >
-          <li className='mb-2'>
-            Get started by editing{' '}
-            <code
-              className={`
-                rounded bg-black/[.05] px-1 py-0.5 font-semibold
-
-                dark:bg-white/[.06]
-              `}
-            >
-              src/app/page.tsx
-            </code>
-            {/*
-             */}
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div
-          className={`
-            flex flex-col items-center gap-4
-
-            sm:flex-row
-          `}
-        >
-          <a
-            className={`
-              flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent
-              bg-foreground px-4 text-sm text-background transition-colors
-
-              dark:hover:bg-[#ccc]
-
-              hover:bg-[#383838]
-
-              sm:h-12 sm:px-5 sm:text-base
-            `}
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <Image className='dark:invert' src='/vercel.svg' alt='Vercel logomark' width={20} height={20} />
-            Deploy now
-          </a>
-          <a
-            className={`
-              flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm
-              transition-colors
-
-              dark:border-white/[.145] dark:hover:bg-[#1a1a1a]
-
-              hover:border-transparent hover:bg-[#f2f2f2]
-
-              sm:h-12 sm:min-w-44 sm:px-5 sm:text-base
-            `}
-            href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className='row-start-3 flex flex-wrap items-center justify-center gap-6'>
+      <div className="flex items-center">
+        <img
+          src="/iocslogo44px.png"
+          alt="IÖCS"
+          className="w-10 h-10 object-contain"
+        />
+      </div>
+      
+      
+      <div className="flex space-x-4">
+    
         <a
-          className={`
-            flex items-center gap-2
-
-            hover:underline hover:underline-offset-4
-          `}
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
+          href="https://www.facebook.com/@instruktorok"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white-600 hover:text-blue-800"
+          aria-label="Facebook"
         >
-          <Image aria-hidden src='/file.svg' alt='File icon' width={16} height={16} />
-          Learn
+          <Facebook className="w-6 h-6" />
         </a>
+
+  
         <a
-          className={`
-            flex items-center gap-2
-
-            hover:underline hover:underline-offset-4
-          `}
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
+          href="https://www.instagram.com/iocs_official/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white-500 hover:text-orange-700"
+          aria-label="Instagram"
         >
-          <Image aria-hidden src='/window.svg' alt='Window icon' width={16} height={16} />
-          Examples
+          <Instagram className="w-6 h-6" />
         </a>
+
         <a
-          className={`
-            flex items-center gap-2
-
-            hover:underline hover:underline-offset-4
-          `}
-          href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
+          href="https://iocs.hu/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white-600 hover:text-yellow-700"
+          aria-label="Privacy Policy"
         >
-          <Image aria-hidden src='/globe.svg' alt='Globe icon' width={16} height={16} />
-          Go to nextjs.org →
+          <img
+          src="/privacy.png"
+          alt="privacy"
+          className="w-6 h-6 object-contain"
+        />
         </a>
-      </footer>
-    </div>
+      </div>
+    </header>
   );
 }
