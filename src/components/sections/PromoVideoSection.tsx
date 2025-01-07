1666ca6369e99a73e032c527127093c885856c94
Bálint Király <balintkiraly.dev@gmail.com>
2025-01-07 08:29:40 +0100

    Add Vimeo Player

--- src/components/sections/PromoVideoSection.tsx
+++ src/components/sections/PromoVideoSection.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import VimeoDefaultPlayer from '@/components/ui/VimeoPlayer';
+
 const PromoVideoSection = () => {
   return (
-    <section className='h-80'>
-      <p>Promo Video</p>
+    <section className='p-4'>
+      <VimeoDefaultPlayer id={Number(process.env.PROMO_VIDEO_VIMEO_ID)} />
     </section>
   );
 };
