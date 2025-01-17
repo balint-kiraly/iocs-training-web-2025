25f3c3992782d30e54dddf36d95dce6444a61764
Bálint Király <balintkiraly.dev@gmail.com>
2025-01-18 00:08:43 +0100

    Add reveal animation

--- src/components/sections/ApplicationSection.tsx
+++ src/components/sections/ApplicationSection.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Reveal } from '@/components/ui/Reveal';
 import { Link } from '@/i18n/routing';
 
 const ApplicationSection = () => {
@@ -11,26 +12,28 @@ const ApplicationSection = () => {
 
   return (
     <div className='px-10 py-20'>
-      <Card
-        className={`
-          mx-auto flex w-fit flex-col items-start gap-8 p-8
+      <Reveal>
+        <Card
+          className={`
+            mx-auto flex w-fit flex-col items-start gap-8 p-8
 
-          sm:flex-row sm:items-center
-        `}
-      >
-        <CardHeader className='p-0'>
-          <CardTitle className='text-2xl'>{text('heading')}</CardTitle>
-          <CardDescription className='text-lg'>{text('subheading')}</CardDescription>
-        </CardHeader>
-        <CardContent className='w-fit p-0'>
-          <Link href='/apply'>
-            <Button variant='primary' size='lg' className='text-lg'>
-              {text('cta')}
-              <Rocket />
-            </Button>
-          </Link>
-        </CardContent>
-      </Card>
+            sm:flex-row sm:items-center
+          `}
+        >
+          <CardHeader className='p-0'>
+            <CardTitle className='text-2xl'>{text('heading')}</CardTitle>
+            <CardDescription className='text-lg'>{text('subheading')}</CardDescription>
+          </CardHeader>
+          <CardContent className='w-fit p-0'>
+            <Link href='/apply'>
+              <Button variant='primary' size='lg' className='text-lg'>
+                {text('cta')}
+                <Rocket />
+              </Button>
+            </Link>
+          </CardContent>
+        </Card>
+      </Reveal>
     </div>
   );
 };
