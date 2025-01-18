84095bfc7b3e541ee95b15ea192dd5bbc3abb4b0
Bálint Király <balintkiraly.dev@gmail.com>
2025-01-18 02:18:11 +0100

    Hero cta buttons

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,17 +1,21 @@
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown, Play, Rocket } from 'lucide-react';
 import Image from 'next/image';
 import { useLocale, useTranslations } from 'next-intl';
 import React from 'react';
 
+import { Button } from '@/components/ui/button';
 import Countdown from '@/components/ui/Countdown';
 import { Reveal } from '@/components/ui/Reveal';
+import { Link } from '@/i18n/routing';
 
 const HeroSection = () => {
   const text = useTranslations('HeroSection');
   const locale = useLocale();
 
   return (
-    <section className='flex h-screen w-screen flex-col items-center justify-end gap-8 overflow-clip text-white'>
+    <section
+      className={`relative flex h-screen w-screen flex-col items-center justify-end gap-8 overflow-clip text-white`}
+    >
       <div className='absolute -z-10 h-screen w-screen overflow-hidden'>
         <Image
           className='relative -top-20'
@@ -48,12 +52,31 @@ const HeroSection = () => {
         <div className='absolute inset-0 bg-gradient-to-t from-black via-black/60 to-transparent' />
       </div>
 
-      <Reveal delay={0.3}>
+      <Reveal delay={0.2}>
         <Countdown targetDate={process.env.COUNTDOWN_TARGET ?? new Date().toDateString()} />
       </Reveal>
 
-      <div className='animation-bouncing-arrow'>
-        <ChevronDown className='mb-4' />
+      <div className='flex flex-wrap items-center justify-center gap-4'>
+        <Reveal delay={0.4}>
+          <Link href='/apply'>
+            <Button variant='primary' size='lg'>
+              {text('cta')}
+              <Rocket />
+            </Button>
+          </Link>
+        </Reveal>
+        <Reveal delay={0.5}>
+          <Link href='/'>
+            <Button variant='light' size='lg' className='bg-background/50'>
+              <Play />
+              {text('cta-video')}
+            </Button>
+          </Link>
+        </Reveal>
+      </div>
+
+      <div className='animation-bouncing-arrow mb-4'>
+        <ChevronDown />
       </div>
     </section>
   );
