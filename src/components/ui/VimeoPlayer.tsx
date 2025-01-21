fac591f0d6205f3772068bc9e41863137c258736
Bálint Király <balintkiraly.dev@gmail.com>
2025-01-21 09:33:30 +0100

    Fallback image

--- src/components/ui/VimeoPlayer.tsx
+++ src/components/ui/VimeoPlayer.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import Player from '@vimeo/player';
+import Image from 'next/image';
 import React, { useEffect, useRef } from 'react';
 
 export default function VimeoPlayer({ id, autoplay }: Readonly<{ id: number; autoplay: boolean }>) {
   const playerRef = useRef<HTMLDivElement>(null);
+  const fallbackImage = useRef<HTMLImageElement>(null);
 
   useEffect(() => {
     const options = {
@@ -16,7 +18,6 @@ export default function VimeoPlayer({ id, autoplay }: Readonly<{ id: number; aut
       portrait: false,
       byline: false,
       title: false,
-      width: 640,
       speed: false,
       color: 'FFFFFF',
       dnt: true,
@@ -25,9 +26,31 @@ export default function VimeoPlayer({ id, autoplay }: Readonly<{ id: number; aut
     };
 
     if (playerRef.current !== null) {
-      new Player(playerRef.current, options);
+      const player = new Player(playerRef.current, options);
+
+      player.on('play', () => {
+        if (fallbackImage.current !== null) {
+          fallbackImage.current.style.opacity = '0';
+        }
+      });
     }
   }, [autoplay, id]);
 
-  return <div ref={playerRef}></div>;
+  return (
+    <>
+      <Image
+        className={`
+          relative z-10 transition-opacity duration-1000
+
+          ${autoplay ? '' : 'hidden'}
+        `}
+        src='/landing-bg-placeholder.jpg'
+        ref={fallbackImage}
+        fill={true}
+        alt='Background fallback image'
+        style={{ objectFit: 'cover' }}
+      />
+      <div ref={playerRef}></div>
+    </>
+  );
 }
