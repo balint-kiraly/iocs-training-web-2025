7b13749ec46a199a877c5f59fa1573c5c173580e
Bálint Király <balintkiraly.dev@gmail.com>
2025-01-27 20:28:31 +0100

    Restore error message alert

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -133,7 +133,7 @@ export const ApplicationForm = () => {
               <Alert variant='destructive' className='mt-10'>
                 <AlertCircle className='h-4 w-4' />
                 <AlertTitle>{text('errors.title')}</AlertTitle>
-                <AlertDescription>{state.message}</AlertDescription>
+                <AlertDescription>{text(`errors.${state.error}`)}</AlertDescription>
               </Alert>
               <p className='mt-2 text-sm text-muted-foreground'>
                 {text.rich('errors.support', {
