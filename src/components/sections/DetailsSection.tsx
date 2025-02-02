06efb3689496caf412f4ae49dc01dd011cd7d0b7
Bálint Király <balintkiraly.dev@gmail.com>
2025-02-02 15:56:08 +0100

    Adjustments to details section

--- src/components/sections/DetailsSection.tsx
+++ src/components/sections/DetailsSection.tsx
@@ -10,25 +10,38 @@ const DetailsSection = () => {
       <Reveal>
         <Card className='max-w-screen-sm'>
           <CardHeader>
-            <CardTitle className='text-2xl font-bold'>{text('f-box-header')}</CardTitle>
+            <CardTitle className='text-3xl font-black'>{text('f-box-header')}</CardTitle>
           </CardHeader>
           <CardContent>
-            <p className='mb-2'>{text('text.f-box-p1')}</p>
-            <p className='mb-2'>{text('text.f-box-p2')}</p>
-            <p className='mb-2'>{text('text.f-box-p3')}</p>
-            <p className='mb-2'>{text('text.f-box-p4')}</p>
-            <p className='mb-2'>{text('text.f-box-p5')}</p>
-            <p>{text('text.f-box-p6')}</p>
+            <ul
+              className={`
+                list-disc pl-6
+
+                marker:text-2xl marker:text-secondary
+              `}
+            >
+              <li className='mb-2'>{text('text.f-box-p1')}</li>
+              <li className='mb-2'>{text('text.f-box-p2')}</li>
+              <li className='mb-2'>{text('text.f-box-p3')}</li>
+              <li className='mb-2'>{text('text.f-box-p4')}</li>
+              <li className='mb-2'>{text('text.f-box-p5')}</li>
+            </ul>
+
+            <p className='mt-6'>{text('text.f-box-p6')}</p>
           </CardContent>
         </Card>
       </Reveal>
       <Reveal delay={0.3}>
         <Card className='max-w-screen-sm'>
           <CardHeader>
-            <CardTitle className='text-2xl font-bold'>{text('s-box-header')}</CardTitle>
+            <CardTitle className='text-3xl font-black'>{text('s-box-header')}</CardTitle>
           </CardHeader>
           <CardContent>
-            <p className='mb-2'>{text('text.s-box-p1')}</p>
+            <p className='mb-2'>
+              {text.rich('text.s-box-p1', {
+                em: (chunks) => <em className='bg-secondary px-2 py-1 rounded'>{chunks}</em>,
+              })}
+            </p>
             <p className='mb-2'>{text('text.s-box-p2')}</p>
             <p>{text('text.s-box-p3')}</p>
           </CardContent>
